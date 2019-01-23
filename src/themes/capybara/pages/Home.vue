654846e73dd2041f3773a95d49a77b45ed699129
Filip Rakowski <frakowski@divante.pl>
2019-01-23 15:11:46 +0100

    margin global classes

--- src/themes/capybara/pages/Home.vue
+++ src/themes/capybara/pages/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="home margin">
-    <div class="top-category-nav margin">
-      <vsf-button full-width color="secondary" class="margin">Women</vsf-button>
-      <vsf-button full-width color="secondary" class="margin">Men</vsf-button>
+  <div id="home margin-big">
+    <div class="top-category-nav margin-small">
+      <vsf-button full-width color="secondary" class="margin-small">Women</vsf-button>
+      <vsf-button full-width color="secondary" class="margin-small">Men</vsf-button>
     </div>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
