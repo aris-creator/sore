391b183985ae1bbd487dd71da654a21ecd218d04
Filip Rakowski <frakowski@divante.pl>
2019-01-23 12:35:28 +0100

    Initial button, scss vars

---
+++ core/modules/storefront-ui/components/Button/Button.ts
@@ -0,0 +1,8 @@
+import { Ripple } from 'vuetify/es5/directives';
+
+export default {
+  name: 'VsfButton',
+  directives: {
+    Ripple
+  }
+}
--- src/themes/capybara/pages/Home.vue
+++ src/themes/capybara/pages/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="home">
     Hello home of Capybara!
+    <vsf-button>Yo</vsf-button>
   </div>
 </template>
 
 <script>
+import VsfButton from '@vue-storefront/core/modules/storefront-ui/components/Button/Button.vue'
+
 export default {
   components: {
+    VsfButton
   },
   computed: {
   },
