30136758fed04fa5301cb84c2355b0dcf7c61f7f
Filip Rakowski <frakowski@divante.pl>
2020-05-18 16:23:14 +0200

    store locator

--- packages/commercetools/theme/components/storyblok/StoreLocator/StoreItem.vue
+++ packages/commercetools/theme/components/storyblok/StoreLocator/StoreItem.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="store-item">
     <img src="logo.svg" />
-    <div class="store-item__info">
-      <h3>Circle K - 272000</h3>
-      <div>300 SaLsbury Ave</div>
-      <div>Spencer, NC - 28159</div>
-    </div>
-    <div class="store-item__opening-hours">
-      <h3>Opening Hours</h3>
-      <div>Mon-Sun</div>
-      <div>5:00AM - 11:00PM</div>
-    </div>
-    <div class="store-item__facilities">
+    <div class="store-item__middle">
+      <div class="store-item__info">
+        <h3>Circle K - 272000</h3>
+        <div>300 SaLsbury Ave</div>
+        <div>Spencer, NC - 28159</div>
+      </div>
+      <div class="store-item__opening-hours">
+        <h3>Opening Hours</h3>
+        <div>Mon-Sun</div>
+        <div>5:00AM - 11:00PM</div>
+      </div>
+      <div class="store-item__facilities">
+        Facilities
+      </div>
     </div>
     <div class="store-item__distance">
       <b>200ft</b> away
@@ -31,18 +34,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@storefront-ui/vue/styles";
+
 .store-item {
   display: flex;
   padding: 20px calc(var(--spacer-xl) * 2);
   justify-content: space-between;
   align-items: center;
   line-height: 140.62%;
+  @include for-mobile {
+    padding: 20px 0;
+    flex-wrap: wrap;
+    align-items: flex-start;
+  }
+}
+.store-item__middle {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 45%;
+  line-height: 140.62%;
+  @include for-mobile {
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    & > div {
+      margin-bottom: 10px;
+    }
+  }
 }
 .store-item__info {
   font-size: 0.875rem;
   text-transform: uppercase;
   h3 {
     font-size: 1rem;
+    margin-bottom: 2px;
   }
 }
 .store-item__opening-hours, .store-item__opening-hours h3 {
@@ -50,6 +76,7 @@ export default {
   font-weight: normal;
   h3 {
     text-transform: uppercase;
+    margin-bottom: 2px;
   }
 }
 .store-item__distance {
@@ -64,6 +91,10 @@ export default {
   border-radius: 24px;
   &:hover {
     background: lighten(#F99B2A, 5%);
+  }
+  @include for-mobile {
+    display: flex;
+    width: 100%;
   }
 }
 </style>
--- packages/commercetools/theme/components/storyblok/StoreLocator/StoreLocator.vue
+++ packages/commercetools/theme/components/storyblok/StoreLocator/StoreLocator.vue
@@ -3,7 +3,7 @@
     <h2>Store Locator</h2>
     <div class="store-locator__search">
       <div class="store-locator__search-box">
-        <SfSearchBar class="store-locator__search-bar"/>
+        <SfSearchBar class="store-locator__search-bar" placeholder="Find your store by City or ZIP" />
       </div>
       <div class="store-locator__search-results">
       <StoreItem />
@@ -27,28 +27,70 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~@storefront-ui/vue/styles";
+
 .store-locator {
   position: relative;
+  padding: 20px var(--spacer-base);
   h2 {
-    margin-left: calc(var(--spacer-xl) * 2); // potentially harmful for perf, get rid of calcs later
+      font-size: 2.25rem;
+  }
+  @include for-desktop {
+    h2 {
+      margin-left: calc(var(--spacer-xl) * 2); // potentially harmful for perf, get rid of calcs later
+    }
   }
 }
 .store-locator__search {
-  box-shadow: 0px 0px 30px rgba(29, 31, 34, 0.08), 1px 10px 10px rgba(102, 62, 51, 0.02);
-  border-radius: 60px;
+  @include for-desktop {
+    box-shadow: 0px 0px 30px rgba(29, 31, 34, 0.08), 1px 10px 10px rgba(102, 62, 51, 0.02);
+    border-radius: 60px;
+  }
 }
 .store-locator__search-box {
-  background: var(--bg-secondary);
-  padding: calc(var(--spacer-xl) * 2);
-  border-radius: 60px;
-  display: flex;
+  @include for-desktop {
+    background: var(--bg-secondary);
+    padding: 3.25rem calc(var(--spacer-xl) * 2);
+    border-radius: 60px;
+  }
+  @include for-mobile {
+    --search-bar-border: 1px solid var(--c-primary);
+    --search-bar-border-width: 2px;
+    margin-top: 20px;
+    box-sizing: initial;
+    input {
+      border-radius: 34px;
+      padding: 20px;
+      padding-right: 30px;
+      color: black;
+      font-weight: bold;
+    }
+    .sf-search-bar__icon {
+      padding-right: 20px;
+    }
+  }
+}
+.store-locator__search-results {
+  padding: var(--spacer-xl) 0;
 }
 .store-locator__search-bar {
-  --search-bar-width: 30rem;
+  @include for-desktop {
+    --search-bar-width: 30rem;
+    input {
+      font-size: 1.2rem;
+      line-height: 140.62%;
+      color: #1D1F22;
+      height: 2.5rem
+    }
+  }
 }
 .store-locator__search-pin {
   position: absolute;
   right: var(--spacer-xl);
   top: 0;
+  @include for-mobile {
+    height: 70px;
+    right: var(--spacer-base);
+  }
 }
 </style>
